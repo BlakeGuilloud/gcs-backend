a7c1220cf90d86051f35c1640923ef71c1930c70
Blake Guilloud <blake.guilloud@gmail.com>
2025-02-15 14:49:28 -0500

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const { default: mongoose } = require('mongoose')
+const app = express()
+const port = 3000
+const service = require('./service')
+
+require('dotenv').config()
+
+app.use(express.json())
+
+app.get('/groceries', async (_, res) => {
+  const list = await service.fetchAll()
+
+  res.send(list)
+})
+
+app.get('/groceries/:id', async (req, res) => {
+  const one = await service.fetchOne(req.params.id)
+
+  res.send(one)
+})
+
+app.post('/groceries', async (req, res) => {
+  const one = await service.postOne(req.body)
+
+  res.send(one)
+})
+
+app.patch('/groceries/:id', async (req, res) => {
+  const one = await service.patchOne(req.params.id, req.body)
+
+  res.send(one)
+})
+
+app.delete('/groceries/:id', async (req, res) => {
+  await service.deleteOne(req.params.id)
+
+  res.send('Success')
+})
+
+app.listen(port, () => {
+  console.log(`GCS app listening on port ${port}`)
+  mongoose.connect(process.env.CONNECTION_STRING)
+})
---
+++ service.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const grocerySchema = new mongoose.Schema({
+  name: String
+})
+
+const Grocery = mongoose.model('Grocery', grocerySchema)
+
+export const fetchAll = () => {
+  return Grocery.find()
+}
+
+export const fetchOne = (id) => {
+  return Grocery.findById(id)
+}
+
+export const postOne = async (data) => {
+  return Grocery.create(data)
+}
+
+export const patchOne = async (_id, data) => {
+  await Grocery.findOneAndUpdate({ _id }, data)
+
+  return fetchOne(_id);
+}
+
+export const deleteOne = async (_id) => {
+  await Grocery.deleteOne({ _id })
+}
